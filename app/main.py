1a819257a016afc3e43792eba3727957f13c6e6a
dipeshp786 <pateldipesh1995@gmail.com>
2025-07-08 13:13:08 -0400

    [any message]

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept()  # wait for client
     connection, _ = server_socket.accept()
+    connection.recv(1024)
     connection.sendall(b"+PONG\r\n")
 
 if __name__ == "__main__":
