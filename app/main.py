38c2c12a0f24c4cf47cf8669222f4e31ff74396c
dipeshp786 <pateldipesh1995@gmail.com>
2025-07-08 13:52:51 -0400

    [any message]

--- app/main.py
+++ app/main.py
@@ -21,6 +21,21 @@ def handle_client_connection(client_socket):
     client_socket.close()
 
 
+from app import DatabaseHandler, ExpirationManager
+
+COMMAND_NAME = "ECHO"
+
+
+def handle_command(
+    args: list, database: DatabaseHandler, expirations_manager: ExpirationManager
+) -> str:
+    if len(args) != 1 or len(args[0]) == 0:  # Error if no argument is provided
+        return "-ERR wrong number of arguments for command\r\n"
+    return f"${len(args[0])}\r\n{args[0]}\r\n"
+    
+    
+    
+
 def main():
     print("Logs from your program will appear here!")
 
