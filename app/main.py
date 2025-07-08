4be9013b8522e05016d2f934fc9b7cbb54a3b62d
dipeshp786 <pateldipesh1995@gmail.com>
2025-07-08 13:27:10 -0400

    [any message]

--- app/main.py
+++ app/main.py
@@ -3,13 +3,18 @@ import socket  # noqa: F401
 def main():
     print("Logs from your program will appear here!")
 
+    # Create server
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()  # wait for client
+
+    # Accept one client connection
     connection, _ = server_socket.accept()
-    connection.receive(1024)
-    connection.sendall(pong.encode)
-    PONG = "+pong\r\n"
-    
+
+    # Receive data (but we don’t use it yet for PING)
+    connection.recv(1024)
+
+    # Send a valid Redis PONG response
+    connection.sendall(b"+PONG\r\n")
+
 
 if __name__ == "__main__":
     main()
