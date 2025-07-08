27394c0ac9e610530177077999caddec9de8c90b
dipeshp786 <pateldipesh1995@gmail.com>
2025-07-08 13:41:29 -0400

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,26 +1,24 @@
-import socket  # noqa: F401
+import socket
 
 def main():
     print("Logs from your program will appear here!")
 
-    # Create server
+    # Create the server socket
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
 
-    # Accept one client connection
+    # Accept a single client connection
     connection, _ = server_socket.accept()
 
-  
+    # Keep responding to multiple PING requests
+    while True:
+        request = connection.recv(1024)
+        if not request:
+            break  # client disconnected
 
-    # Send a valid Redis PONG response
-    connection.sendall(b"+PONG\r\n")
+        data = request.decode()
 
-while True:
-        request: bytes = client_socket.recv(512)
-        data: str = request.decode()
-
-        # print(data)
         if "ping" in data.lower():
-            client_socket.send("+PONG\r\n".encode())
+            connection.sendall(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
